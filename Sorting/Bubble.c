a0ff0652c4f0bdff2fd2fafd30bb5a7bc9468b01
Krittin Chaowakarn <130139478+krittin-ch@users.noreply.github.com>
2024-01-23 09:51:27 +0700

    Upload

--- Sorting/Bubble.c
+++ Sorting/Bubble.c
@@ -7,18 +7,21 @@ int main() {
     while (s != 0) {
         s = 0;
         for (int i = 0; i < sizeof(arr)/sizeof(int) - 1; i++) {
-        if (arr[i] > arr[i+1]) {
-            int a = arr[i];
-            arr[i] = arr[i+1];
-            arr[i+1] = a;
-            s += 1;
+            if (arr[i] > arr[i+1]) {
+                int a = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = a;
+                s += 1;
+                }
             }
-        }
+        
         printf("%d", s);
         printf("\n");
+        
         for (int i = 0; i < sizeof(arr)/sizeof(int); i++) {
             printf("%d ", arr[i]);
             }
+
         printf("\n");
     }
     return 0;
