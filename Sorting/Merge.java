d4dff26b1ee9dfee38b61f5ffa8cfa78998a11a9
Krittin Chaowakarn <130139478+krittin-ch@users.noreply.github.com>
2024-02-02 07:03:26 +0700

    Upload

--- Sorting/Merge.java
+++ Sorting/Merge.java
@@ -2,8 +2,20 @@ package Sorting;
 
 public class Merge {
 
-    public void MergeSort(int[] arr) {
+    public void merge(int arr[], int l, int m, int r)
+    {
+        int n1 = m - l + 1;
+        int n2 = r - m;
 
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        for (int i=0; i<n1; i++) {
+            L[i] = arr[l+i];
+        }
+        for (int j=0; j<n2; j++) {
+            R[j] = arr[m+1+j];
+        }
     }
 
     public static void main(String[] args) {
